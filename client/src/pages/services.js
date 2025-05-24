cc811a6b41f9644d2edadfde9b58b866e4e7c9f7
protikka1 <protikka917@gmail.com>
2025-05-24 09:11:52 -0700

    Create services.js

---
+++ client/src/pages/services.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Services = () => (
+  <div>
+    <h2>Our Services</h2>
+    <ul>
+      <li>Housing Support</li>
+      <li>Document Retrieval</li>
+      <li>Clean & Organize Living Spaces</li>
+      <li>Online Account Management</li>
+    </ul>
+    <p>Need help? <a href="/intake-form">Fill out our intake form</a></p>
+  </div>
+);
+
+export default Services;

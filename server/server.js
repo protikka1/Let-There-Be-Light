c3ae9d666f902c4ea26759c3ff0a9788b33c75ed
Abul Islam <1234577>
2025-05-30 03:23:21 -0700

    added file client/package.json

--- server/server.js
+++ server/server.js
@@ -1,1 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+// MongoDB connection
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
+
+// Routes
+app.use('/api/services', require('./routes/services'));
+
+app.get('/', (req, res) => {
+  res.send('Let There Be Light backend is running');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
